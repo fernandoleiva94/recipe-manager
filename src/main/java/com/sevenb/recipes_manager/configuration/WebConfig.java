ddf6339740d58386dfd86ef7aae3294b3419ad08
fernandoleiva94 <TuNuevoEmail@example.com>
2025-06-17 23:51:48 -0300

    updaye properties

--- src/main/java/com/sevenb/recipes_manager/configuration/WebConfig.java
+++ src/main/java/com/sevenb/recipes_manager/configuration/WebConfig.java
@@ -1,16 +1,20 @@
 package com.sevenb.recipes_manager.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${recipe.manager.service.recipeFrontUrl}")
+    private String recipeFrontUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**") // Ajusta la ruta según sea necesario
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://d4bf-190-192-90-146.ngrok-free.app")// Ajusta esto si tu frontend está en otro lugar
+                .allowedOrigins(recipeFrontUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
